967ea0a3cb31979b5eac1298175c25d5d5c23922
Alex Ranga <alexisranga@gmail.com>
2022-02-28 08:44:01 -0800

    completed week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 2: Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,56 +15,170 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
+// const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
+describe('mult3', () => {
+    it('take in an array of numbers and returns an array with all the numbers multiplied by 3', () => {
+        const numbersArray1 = [6, 7, 8, 9, 10]
+        // Expected output: [18, 21, 24, 27, 30]
+        const numbersArray2 = [24, 27, 30, 33, 36]
+        // Expected output: [72, 81, 90, 99, 108]
+
+        expect(mult3(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+        expect(mult3(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+    })
+})
+// Good Failure
+
+// PSEUDO CODE:
+// create a function named mult3
+// mult3 will take in a parameter of an array named array
+// will iterate through that array and return a new array with each element multiplied by three
+// will then return that new array
 
 // b) Create the function that makes the test pass.
 
+// will iterate through that array and return a new array with each element multiplied by three
+ const mult3 = array => array.map(value => value * 3)
+
+// Test Passed
 
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
+//const num1 = 15
 // Expected output: "15 is divisible by three"
-const num2 = 0
+//const num2 = 0
 // Expected output: "0 is divisible by three"
-const num3 = -7
+//const num3 = -7
 // Expected output: "-7 is not divisible by three"
+
+describe('divisibleBy3', () => {
+    it('take a number as an argument and decides if the number is evenly divisible by three or not', () => {
+        const num1 = 15
+        // Expected output: "15 is divisible by three"
+        const num2 = 0
+        // Expected output: "0 is divisible by three"
+        const num3 = -7
+        // Expected output: "-7 is not divisible by three"
+
+        expect(divisibleBy3(num1)).toEqual("15 is divisible by three")
+        expect(divisibleBy3(num2)).toEqual("0 is divisible by three")
+        expect(divisibleBy3(num3)).toEqual("-7 is not divisible by three")
+    })
+})
+
+// Good failure
+
+// PSEUDO CODE for a & b:
+// create a function named divisibleBy3
+// divisibleBy3 will have a parameter of number - which will be a number
+// will check if that number is divisible by three or not
+// will return a string with the number with the remarks "is divisble by three", " is not divisible by three"
+
 
 
 // b) Create the function that makes the test pass.
 
+// will check if that number is divisible by three or not
+// will return a string with the number with the remarks "is divisble by three", " is not divisible by three"
+
+const divisibleBy3 = number => number % 3 === 0 ? `${number} is divisible by three` : `${number} is not divisible by three`
+
+// Test Passed
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+describe('capitalizedWords', () => {
+    it('take in an array of words and returns an array with all the words capitalized', () => {
+        const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
+
+        expect(capitalizedWords(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+        expect(capitalizedWords(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+})
+
+// Good failure
+
+// PSEUDO CODE for a & b:
+// create a function named capitalizedWords
+// capitalizedWords will take in an array of strings named array
+// will loop over the array and transform the first letter in the string to uppercase for each element using the buil-in methods toUpperCase and .charAt
+// get just the first letter and capitalizing it and then add the rest of the string (except the first letter)
+// will then return the new array of capitalized words
 
 
 // b) Create the function that makes the test pass.
 
 
+
+const capitalizedWords = array => array.map(value => value.charAt(0).toUpperCase() + value.slice(1))
+
+// Test passed
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
+//const vowelTester1 = "learn"
 // Expected output: 1
-const vowelTester2 = "academy"
+//const vowelTester2 = "academy"
 // Expected output: 0
-const vowelTester3 = "challenges"
+//const vowelTester3 = "challenges"
 // Expected output: 2
 
+describe('indexOfFirstVowel', () => {
+    it('take in a string and logs the index of the first vowel', () => {
+        const vowelTester1 = "learn"
+        // Expected output: 1
+        const vowelTester2 = "academy"
+        // Expected output: 0
+        const vowelTester3 = "challenges"
+        // Expected output: 2
+
+        expect(indexOfFirstVowel(vowelTester1)).toEqual(1)
+        expect(indexOfFirstVowel(vowelTester2)).toEqual(0)
+        expect(indexOfFirstVowel(vowelTester3)).toEqual(2)
+        
+    })
+})
+
+// Good failure
+
+// PSEUDO CODE for a & b:
+// create a function named indexOfFirstVowel to hold the value for the indexes of the first vowels
+// firstIndexOfVowel will take in a string named string
+// define what vowels are in the created function
+// iterate through each character in the string
+// determine if the character is a vowel by converting character to lowercase and comparing with our defined vowels
+// this will handle any uppercase letters that may be passed to our function
+// find the first occurrence of a vowel and return it
 
 // b) Create the function that makes the test pass.
+
+
+
+
+const indexOfFirstVowel = string => {
+    const vowels = 'aeiou'
+
+    return string.split('').findIndex(value => vowels.includes(value.toLowerCase()))
+}
+
+// Test passed
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,9 +10,9 @@
 var cohort = "Alpha 2022"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: ['A', 'l', 'p', 'h', 'a', ' ', '2', '0', '2', '2']
 
+// b) Verify and explain: Correct - The built-in method .split() divides a string into an ordered list of substrings (individual characters). These substrings are put into an array and returns it as an array.
 
 // --------------------2) What will this log?
 
@@ -21,17 +21,18 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+
+// a) Your answer: Undefined
+// b) Verify and explain: Correct - This is an incomplete function. It's missing the "Return" block which is the output of a function. Without a Return, a function will log Undefined.
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+//  console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: Correct - .map() will do something to each item in an array and return an array of the same length. For this case, each value is multiplied  by 2.
 
 
 // --------------------4) What will this log?
@@ -39,8 +40,8 @@ var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
 // console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [11, 13, 15]
+// b) Verify and explain:Correct - .filter() will make a decision about each item in an array and return a subset based on the criteria given. For this case, only ODD numbers are returned in the array.
 
 
 // --------------------5) What will this log?
@@ -53,8 +54,8 @@ var myCodingSkills = {
 }
 // console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: Correct - the concept of Object Destructuring was used and it allows you to take something like an array or an object and unpack their values into individual variables. When destructuring an object you are essentially taking the properties of an object and breaking them out into individual, recallable variables. Breakdown: myCodingSkills into languuages, into [0] Javascript. 
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +68,7 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: {student: 'George', cohort: 'Alpha', year: 2022 }
+// b) Verify and explain:Correct - Classes are a particular type of function that contain data and behavior which make it the blueprint for an object. When an object is being created from this blueprint, we are giving it an argument that "George" will be assigned to the student property. The constructor method was used for creating and initializing the obejct of that particular class.
